49759a26b272f1ee21f4d9aa3b21a13a4e02f5cc
Ruslan Mamedov <Ruslan.I.Mamedov@gmail.com>
2023-09-12 19:03:50 +0000

    Initial ansible commit

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,11 +1,11 @@
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source  = "yandex-cloud/yandex"
-#      version = "0.95.0"
-#    }
-#  }
-#}
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.95.0"
+    }
+  }
+}
 
 provider "yandex" {
   token     = "y0_AgAAAAABfS9HAATuwQAAAADrnG4qDVpf6JTWQJ2qehT8AZeuoeDBrEY"
